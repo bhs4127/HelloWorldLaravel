9f5cf3968df3e18486c34a7ad08c7999f0ddcbee
vagrant <vagrant@homestead>
2019-11-13 23:58:02 +0000

    init commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello World</title>
+
+    </head>
+    <body>
+
+        <div>
+            <?php
+            $currentDate = date("l, F jS");
+            echo "Hello World... it's $currentDate";
+            ?>
+        </div>
+
+    </body>
+</html>
